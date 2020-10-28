3a2a53f425c20c71f55c418acd1745308b9ff92c
Anthony Leon <acidknights89@gmail.com>
2020-10-28 19:12:49 +0000

    Added requirements and gems to environment.rb

---
+++ CLI_Class_Creator/environment.rb
@@ -0,0 +1,8 @@
+require_relative "./lib/api.rb"
+require_relative "./lib/cli.rb"
+require_relative "./lib/classes.rb"
+require_relative "./lib/races.rb"
+
+require 'pry'
+require 'json'
+require 'net/http'
