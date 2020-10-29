f29b1e2f64ab8e3652d4a0955d13c23a6089e1bb
Anthony Leon <acidknights89@gmail.com>
2020-10-29 01:41:54 +0000

    Added small changes to the API class/saving progress.

--- lib/api.rb
+++ lib/api.rb
@@ -3,12 +3,14 @@ class API
   def self.fetch_classes
     #binding.pry
     url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-    #binding.pry
-    uri = URI(url)
     binding.pry
+    uri = URI(url)
     response = Net::HTTP.get(uri)
     hash = JSON.parse(response)
+
+    array_of_products = hash("name")
   end
+
 
 
 end
