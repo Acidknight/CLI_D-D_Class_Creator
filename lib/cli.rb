8e8a17d15325082953ce03a00de3de61d8e356ee
Anthony Leon <acidknights89@gmail.com>
2020-10-29 06:23:49 +0000

    Built the menu methos within cli.rb.

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,20 @@ class CLI
     #binding.pry
     puts "Welcome!"
     API.fetch_products
+    self.menu
+  end
+
+  def menu
+    puts "Would you like to see the list of Maybelline products?"
+    puts "\n"
+    puts "Type 'yes' or 'y' to continue, or type anything else to exit."
+
+    user_input = gets.strip.downcase
+
+    if user_input == "yes" || user_input == "y"
+      puts "Confirmed, the products will come up shortly."
+    end
+
   end
 
 end
