08885ee3cbc35e6d4d4aa947b868d2b5b4c60008
Anthony Leon <acidknights89@gmail.com>
2020-10-29 02:04:58 +0000

    Changed the name of fetch_classes.

--- lib/api.rb
+++ lib/api.rb
@@ -3,12 +3,19 @@ class API
   def self.fetch_classes
     #binding.pry
     url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-    binding.pry
+    #binding.pry
     uri = URI(url)
     response = Net::HTTP.get(uri)
     hash = JSON.parse(response)
 
     array_of_products = hash
+
+    array_of_products.each do |product_hash|
+      #binding.pry
+      makeup = Makeup.new
+      name = product_hash["name"]
+
+    end
   end
 
 
